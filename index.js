92857cc60c79e97fb6cfa178c9a7fe1ee85d343c
unknown <quiramapino347@gmail.com>
2020-07-13 16:48:37 -0500

    actualizaciones7

---
+++ index.js
@@ -0,0 +1,98 @@
+"use strict";
+
+/*let numberOne = 60;
+let numberTwo = 80;
+let result = numberOne + numberTwo;
+
+console.log(result);
+
+
+let name = "jhon";
+let lastname = "carter";
+let completename = name + ' ' + lastname
+
+console.log(completename)*/
+
+/*let numberOne=100;
+let numberTwo=600;
+
+let result2= numberOne <= numberTwo;
+
+console.log(result2)*/
+
+/*let passwordDb = "pepe123";
+
+let input = "pepe123";
+
+let result = input == passwordDb;
+
+if (result == true){
+    console.log("login correcto");
+}else{
+    console.log("contraseña incorrects")
+}*/
+
+/*let score = 70;
+
+if (score > 30){
+console.log("you need to practice english");
+}else if(score > 15){
+console.log("estas mejorando");
+}
+else{
+console.log("you need to follow to tutotial");
+}*/
+
+/*let typecard = "red";
+
+switch (typecard) {
+  case "debid card":
+    console.log("this card debit");
+    break;
+  case "credit card":
+    console.log("credit card");
+    break;
+  default:
+    console.log("no card");
+}*/
+
+/*let count = 50;
+
+while(count > 0){
+    console.log("hello word");
+    count = count - 1;
+}
+*/
+
+/*let count = 0;
+
+while(count <= 50){
+    console.log(count);
+    count ++;
+}*/
+
+/*let names = ["ryan","bryan","ritan","shakira"];
+/*console.log(names[0]);*/
+
+/*for (let i = 0 ; i < names.length;i++){
+ console.log(names[i]);
+}*/
+
+/*for (let i=names.length - 1;i >0 ;i--){
+    console.log(names[i])
+}*/
+
+/*function greeting(person){
+    let variables =1;
+    let variables2=2;
+    let result= 1+2;
+console.log("hello" + " " +  person);
+}
+greeting("markus");*/
+
+function add (n1,n2){
+console.log(n1+n2);
+
+}
+
+add(3,6);
